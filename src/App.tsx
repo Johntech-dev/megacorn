7a4b2840058c3cf0f306a1be88f53369e949bf6a
johntech-dev <johnayomide50@gmail.com>
2023-07-29 11:28:02 +0100

    Section 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Navbar from './Component/nav/Navbar';
-
+import Sectionhead from './Component/Section1/Sectionhead';
 function App() {
   return (
     <div>
       <Navbar />
+      <Sectionhead />
     </div>
   );
 }
--- src/Component/Section1/Sectionhead.tsx
+++ src/Component/Section1/Sectionhead.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
+import './Sectionhead.css';
+import Lauchmodal from '../Lauchproducts/Lauchmodal';
 
 const Sectionhead = () => {
+  const [showLauchmodal, setshowLauchmodal] = useState(false)
+
   return (
-    <div>
-      
+    <div className='attribute'>
+    <div className='header'>
+      <div className='header-text1'>
+        <h1 className='headtext'>Find and launch <mark className='blue'>digital</mark></h1>
+        <h1 className='headtext'><mark className='blue'>Products</mark> for Africa</h1>
+        <p className='sub-text'>We connect creators & teams to their desired market of customers, partners.<br/>talents and investors needed to build great companies.</p>
+        <div className='btn'>
+      <button type='button' className='btn-1' onClick={() => {setshowLauchmodal(true)}}>Lauch my Products</button>
+      <button type='button' className='btn-2'>Explore Product</button>
+    </div>
+      </div>
+      </div>
+    {
+            showLauchmodal &&
+            <Lauchmodal onClose={() => {setshowLauchmodal(false) }}/>
+          }
+
     </div>
   )
 }
 
-export default Sectionhead
+export default Sectionhead;
